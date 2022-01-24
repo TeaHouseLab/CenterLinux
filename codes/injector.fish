7039c4033effd6878a3a001207d4155c7e42d947
tsingkwai <tsingkwai@bluelaketable>
2022-01-24 11:04:50 +0800

    2022-01-24_03:04:50

--- codes/injector.fish
+++ codes/injector.fish
@@ -8,17 +8,17 @@ function injector
     logger 0 "you don't need to type a space before the first arg"
     echo "[0]core (basefile like id package,tools,etc)
 [1]advance (install bootup splash and grub themes)
-[2]desktop(install recommended themes and layout for your gnome/kde desktop)
+[2]desktop(install a full set of centerlinux)
 [-1]exit injector"
-    read -P "$prefix >>> " args
+    read -P "$prefix >>> " args -a
     for target in $args
         switch $target
             case 0
-                ctpkg ctpm grab center-id ctcontainer ctpkg neofetch progynosh
+                ctpkg grab upg && ctpkg grab center-id ctcontainer ctpkg neofetch progynosh ctutils
             case 1
-                ctpkg ctpm grab center-plymouth-theme center-grub-theme
+                ctpkg grab upg && ctpkg grab center-plymouth-theme center-grub-theme enter-id ctcontainer ctpkg neofetch progynosh ctutils
             case 2
-                ctpkg ctpm grab center-id-desktop chromeos-gtk-theme matcha-kde-wallpapers papirus-icon-theme
+                ctpkg grab upg && ctpkg grab center-id-desktop chromeos-gtk-theme matcha-kde-wallpapers papirus-icon-theme center-kde-theme grab center-plymouth-theme center-grub-theme enter-id ctcontainer ctpkg neofetch progynosh ctutils
             case "*"
                 logger 4 "Unexpect Input,abort"
                 exit
