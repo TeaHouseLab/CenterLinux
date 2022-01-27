a66783be0bad0dfa31473e6cf06b271af888d4be
tsingkwai <tsingkwai@bluelaketable>
2022-01-27 18:24:51 +0800

    2022-01-27_10:24:51

--- codes/injector.fish
+++ codes/injector.fish
@@ -3,12 +3,13 @@ function injector
     else
         init
     end
-    logger 0 "Welcome using CenterLinux Injector,choose what package you want to flash in the following menu,use space with 2+ args,For example"
+    logger 0 "Welcome using CenterLinux Injector,choose what package you want to flash in the following menu"
     logger 0 ">>> 0 1"
     logger 0 "you don't need to type a space before the first arg"
     echo "[0]core (basefile like id package,tools,etc)
 [1]advance (install bootup splash and grub themes)
-[2]desktop(install a full set of centerlinux)
+[2]desktop for kde(install a full set of centerlinux)
+[3]desktop for gnome(install a full set of centerlinux)
 [-1]exit injector"
     read -P "$prefix >>> " args -a
     for target in $args
@@ -18,7 +19,9 @@ function injector
             case 1
                 ctpkg grab upg && ctpkg grab center-plymouth-theme center-grub-theme enter-id ctcontainer ctpkg neofetch progynosh ctutils
             case 2
-                ctpkg grab upg && ctpkg grab center-id-desktop chromeos-gtk-theme matcha-kde-wallpapers papirus-icon-theme center-kde-theme grab center-plymouth-theme center-grub-theme enter-id ctcontainer ctpkg neofetch progynosh ctutils
+                ctpkg grab upg && ctpkg grab center-id-desktop papirus-icon-theme center-kde-theme center-plymouth-theme center-grub-theme enter-id ctcontainer ctpkg neofetch progynosh ctutils
+            case 3
+                ctpkg grab upg && ctpkg grab center-id-desktop center-plymouth-theme center-grub-theme enter-id ctcontainer ctpkg neofetch progynosh ctutils center-gnome-theme 
             case "*"
                 logger 4 "Unexpect Input,abort"
                 exit
